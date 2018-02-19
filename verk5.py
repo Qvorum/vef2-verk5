8bd3014d080b37eac004635dfa5c895a18ddf32a
Qvorum <hakonha213@nemi.tskoli.is>
2018-02-19 11:45:19 +0000

    Add files via upload

---
+++ verk5.py
@@ -0,0 +1,15 @@
+from bottle import *
+import urllib.request, json
+
+with urllib.request.urlopen("http://apis.is/concerts") as url:
+    gogn = json.loads(url.read().decode())
+
+#print(data)
+#for i in gogn['results']:
+#    print(i['eventDateName'])
+
+@route('/')
+def index():
+    return template('index.tpl', data=gogn)
+
+run()
